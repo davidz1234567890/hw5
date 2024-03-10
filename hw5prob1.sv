4e202486c090c7fffab5d4e257ac6094e9d2439e
davidz1234567890 <129808533+davidz1234567890@users.noreply.github.com>
2024-03-10 14:44:37 -0400

    Update hw5prob1.sv

--- hw5prob1.sv
+++ hw5prob1.sv
@@ -5,9 +5,6 @@ output logic found_it);
 enum logic [2:0] {nothing = 3'b001, saw1 = 3'b010, 
                     saw0 = 3'b100} state, nextState;
 always_comb begin
-  /*nextState[2] = state[1] & (~a);
-  nextState[1] = (~state[0]) & a | state[0] & (~a);
-  nextState[0] = state[0] & a;*/
   unique case (state)
     nothing: nextState = (a ==1) ? saw1 : nothing;
     saw1: nextState = (a ==1) ? saw1 : saw0;
@@ -15,7 +12,11 @@ always_comb begin
   endcase
 end
 always_comb begin
-  found_it = (state == saw0) && (a==1);
+  unique case (state)
+    nothing: found_it = 0;
+    saw1: found_it = 0;
+    saw0: found_it = (a==1) ? 1 : 0;
+  endcase
 end
 always_ff @(posedge clock, negedge reset_L)
   if (~reset_L)
@@ -85,6 +86,9 @@ module tb2(output logic clock, reset_L, a,
     a <= 1;
     @(posedge clock);
     a <= 0;
+    @(posedge clock);
+    a <= 1;
+    @(posedge clock);
     $finish;
 
 
